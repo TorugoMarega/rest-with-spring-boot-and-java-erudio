199d85b5e8c936dee7254cea2611cdf1fe6f1040
Vhmarega <victorhugomarega@hotmail.com>
2022-07-15 18:02:40 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/torugo/primeiroprojetospring/RestWithSpringBootAndJavaErudioApplication.java
@@ -0,0 +1,14 @@
+package br.com.torugo.primeiroprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaErudioApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/br/com/torugo/primeiroprojetospring/RestWithSpringBootAndJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.torugo.primeiroprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
